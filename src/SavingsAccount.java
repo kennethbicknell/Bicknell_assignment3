c0a5d2629400c6393d4314dd02b4a49f3f2b4a62
kenneth <kenneth.bicknell@knights.ucf.edu>
2020-02-22 19:05:11 -0500

    set up all interest logic

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,3 +1,23 @@
 public class SavingsAccount{
+    private static double annualInterestRate;
+    private double savingsBalance;
 
+    public SavingsAccount(double interestRate, double initialDeposit){
+        this.savingsBalance = 0;
+        modifyInterestRate(interestRate);
+        deposit(initialDeposit);
+    }
+
+    public void modifyInterestRate(double rate){
+        annualInterestRate = rate;
+    }
+
+    public void calculateMonthlyInterest(){
+        double monthlyInterestRate = annualInterestRate / 12;
+        deposit(this.savingsBalance * monthlyInterestRate);
+    }
+
+    private void deposit(double amount){
+        this.savingsBalance += amount;
+    }
 }
